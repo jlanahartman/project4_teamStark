2b63b5c432e2f41811e3f797cb9701e41c84b9d2
matthewc <122582438+SolSolstice@users.noreply.github.com>
2023-07-04 18:07:54 -0400

    Delete wine_website directory
    cleaning - removing old files

--- wine_website/main.py
+++
@@ -1,9 +0,0 @@
-from website import create_app
-
-app = create_app() 
-
-if __name__ == '__main__': # only if we run this file - we will execute 
-    app.run(debug=True) # this -- allows us to automatically run the server
-
-
-
